b0adaf5e1cc20bf7179f4c74d578b9b4a63a9ada
avadhootech711 <avadhootechp.711@gmail.com>
2026-01-12 12:00:45 +0530

    C Programming Assignment

---
+++ Assignments/Assignment_4/program4_4.c
@@ -0,0 +1,74 @@
+///////////////////////////////////////////////////////////////
+//
+//  Required Header Files
+//
+///////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+//  Function Name : SumNonFact
+//  Input         : Integer
+//  Output        : Integer
+//  Description   : It accepts a number from the user and returns 
+//                  the summation of all numbers that are NOT factors of it.
+//  Author        : Avadhoot Subhash Pawar
+//  Date          : 27/10/2025
+//
+///////////////////////////////////////////////////////////////
+
+int SumNonFact(int iNo)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    if(iNo < 0)                                             // Business Logic
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if(iNo % iCnt != 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+
+    return iSum;
+
+}   // End of SumNonFact
+
+///////////////////////////////////////////////////////////////
+//
+//  Entry point function of application
+//
+///////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d", &iValue);
+
+    iRet = SumNonFact(iValue);
+
+    printf("%d\n", iRet);
+
+    return 0;
+
+}   // End of main
+
+///////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 12       Output : 50
+//  Input : 10       Output : 37
+//  Input : -10      Output : 37
+//  Input : 13       Output : 78
+//
+///////////////////////////////////////////////////////////////
