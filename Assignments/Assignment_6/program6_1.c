7b50bdf31e8f0b89ba5ad1b4e1348cdf92848895
avadhootech711 <avadhootechp.711@gmail.com>
2026-01-13 10:38:27 +0530

    C Programming Assignments

---
+++ Assignments/Assignment_6/program6_1.c
@@ -0,0 +1,58 @@
+///////////////////////////////////////////////////////////////
+//
+//  Required Header Files
+//
+///////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////
+//
+//  Function Name : DisplayName
+//  Input         : None(String)
+//  Output        : None(Void)
+//  Description   : It accepts a full name from the user and 
+//                  displays it on the screen.
+//  Author        : Avadhoot Subhash Pawar
+//  Date          : 28/10/2025
+//
+///////////////////////////////////////////////////////////////
+
+void DisplayName()
+{
+    char cName[30];
+
+    printf("Please enter full name: ");
+    scanf(" %[^\n]", cName);
+
+    printf("Your name is %s", cName);
+
+}   // End of DisplayName
+
+// TIME COMPLEXITY : O(n)
+
+///////////////////////////////////////////////////////////////
+//
+//  Entry point function of application
+//
+///////////////////////////////////////////////////////////////
+
+int main()
+{
+    DisplayName();
+
+    return 0;
+
+}   // End of main
+
+///////////////////////////////////////////////////////////////
+//
+//  Test Cases successfully handled by the application
+//
+//  Input : Piyush Khairnar     Output : Your name is Piyush Khairnar
+//  Input : Avadhoot Pawar      Output : Your name is Avadhoot Pawar
+//  Input : Rohit Sharma        Output : Your name is Rohit Sharma
+//  Input : Virat Kohli         Output : Your name is Virat Kohli
+//  Input : Ajit Pawar          Output : Your name is Ajit Pawar
+//
+///////////////////////////////////////////////////////////////
