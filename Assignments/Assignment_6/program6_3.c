c3075261f51d27183d981f0afe3abea507e2816a
avadhootech711 <avadhootechp.711@gmail.com>
2026-01-13 10:36:17 +0530

    C Programming Assignment files

---
+++ Assignments/Assignment_6/program6_3.c
@@ -0,0 +1,76 @@
+///////////////////////////////////////////////////////////////
+//
+//  Required Header Files
+//
+///////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdbool.h>
+
+///////////////////////////////////////////////////////////////
+//
+//  Function Name : CheckEqual
+//  Input         : Integer, Integer
+//  Output        : Boolean
+//  Description   : This function checks whether two given numbers
+//                  are equal or not.
+//  Author        : Avadhoot Subhash Pawar
+//  Date          : 28/10/2025
+//
+///////////////////////////////////////////////////////////////
+
+bool CheckEqual(int iNo1, int iNo2)
+{
+    if (iNo1 == iNo2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}   // End of CheckEqual
+
+//  TIME COMPLEXITY : O(1)
+
+///////////////////////////////////////////////////////////////
+//
+//  Entry point function of application
+//
+///////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    bool bRet = false;
+
+    printf("Please Enter two numbers : ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    bRet = CheckEqual(iValue1, iValue2);
+
+    if (bRet == true)
+    {
+        printf("Equal");
+    }
+    else
+    {
+        printf("Not Equal");
+    }
+
+    return 0;
+
+}   // End of main
+
+///////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 10 10       Output : Equal
+//  Input : 10 12       Output : Not Equal
+//  Input : 10 -10      Output : Not Equal
+//  Input : -10 -10     Output : Equal
+//  Input : 11 11       Output : Equal
+//
+///////////////////////////////////////////////////////////////
